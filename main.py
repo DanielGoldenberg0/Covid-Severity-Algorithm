8fe48084992a9ac45e1cd0a0d6a52c2cacbc7cc3
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2020-08-02 14:44:35 -0400

    Created files

---
+++ main.py
@@ -0,0 +1,20 @@
+import xlrd
+import xlwt
+
+PATH = "test.xlsx"
+
+
+def mildSeverity():
+    pass
+
+
+def mildWithRiskSeverity():
+    pass
+
+
+def moderateSeverity():
+    pass
+
+
+def severeSeverity():
+    pass
