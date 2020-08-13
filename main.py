47e61c1b6a9b1f3782625eb8ce480a9a61a74918
DanielGoldenberg <goldenbergdaniel8@gmail.com>
2020-08-13 13:04:43 -0400

    Worked on data reading

--- main.py
+++ main.py
@@ -1,50 +1,62 @@
 import xlrd
 import xlwt
 
+from patient import Patient
+
 PATH = "test.xlsx"
 
-workbook = xlrd.open_workbook(PATH)
-worksheet = workbook.sheet_by_index(0)
+wb = xlrd.open_workbook(PATH)
+worksheet = wb.sheet_by_index(0)
 
-patients = [
-    {
-        "id": {
-            "symptoms": ["", "", ""],
-            "age": "",
-            "mild_signs": [],
-            "intubated": "",
-            "moderate_signs": [],
-            "severe_signs": []
-        }
-    }
-]
+
+patients: Patient = []
 
 
 def read_data():
     for row in range(worksheet.nrows):
-        if worksheet.cell_value(row, 0) != "ID":
-            pass
-
-    for val in ids:
-        for col in range(worksheet.ncols):
-            pass
-
+        if worksheet.cell_value(row, 0) != "Pt's #":
+            patient = Patient()
+            patients.append(patient)
+            for col in range(worksheet.ncols):
+                if "Symptom" in worksheet.cell_value(0, col) and worksheet.cell_value(row, col) != "na":
+                    patients[row-1].symptoms.append(worksheet.cell_value(row, col))
+                elif "Age" in worksheet.cell_value(0, col):
+                    patients[row-1].age = int(worksheet.cell_value(row, col))
+                elif ("CAD" in worksheet.cell_value(0, col)
+                    or "DM" in worksheet.cell_value(0, col)
+                    or "HTN" in worksheet.cell_value(0, col)
+                    or "Transplant" in worksheet.cell_value(0, col)
+                    or "Immunosuppresion" in worksheet.cell_value(0, col)):
+                    patients[row-1].mild_signs.append(worksheet.cell_value(row, col))
+                elif "Intubated" in worksheet.cell_value(0, col):
+                    if worksheet.cell_value(row, col).lower() == "yes":
+                        patients[row-1].intubated = True
 
 def write_result():
     pass
 
 
-def mild_severity():
+def mild_severity(patient: Patient) -> bool:
     return False
 
 
-def mild_with_risk_severity():
+def mild_with_risk_severity(patient: Patient) -> bool:
     return False
 
 
-def moderate_severity():
+def moderate_severity(patient: Patient) -> bool:
     return False
 
 
-def severe_severity():
+def severe_severity(patient: Patient) -> bool:
     return False
+
+
+read_data()
+for patient in patients:
+    patient: Patient
+    print(patient.symptoms)
+    print(patient.age)
+    print(patient.mild_signs)
+    print(patient.intubated)
+    print("-----------------------------------------------")
---
+++ patient.py
@@ -0,0 +1,8 @@
+class Patient:
+    def __init__(self):
+        self.symptoms = []
+        self.age = 0
+        self.mild_signs = []
+        self.intubated = False
+        self.moderate_signs = []
+        self.severe_signs = []
